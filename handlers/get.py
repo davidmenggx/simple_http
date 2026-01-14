41a25c94a49c1ea709c0b17ae6e73e176742e754
David Meng <davidmenggx@gmail.com>
2026-01-13 16:06:38 -0800

    Got caching to work

--- handlers/get.py
+++ handlers/get.py
@@ -1,3 +1,4 @@
+import hashlib
 from pathlib import Path
 from datetime import datetime, timezone
 
@@ -19,6 +20,13 @@ def get(path: str, headers: dict[str,str]) -> bytes:
                         content = f.read()
 
                         now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
+                        
+                        sha256_hash = hashlib.sha256()
+                        sha256_hash.update(content)
+                        current_etag = sha256_hash.hexdigest()
+                        user_etag = headers.get('if-none-match')
+                        if user_etag and user_etag[1:-1] == current_etag:
+                                return (f'HTTP/1.1 304 Not Modified\r\nDate: {now}\r\n').encode('utf-8')
 
                         response = (f'HTTP/1.1 200 OK\r\nDate: {now}\r\n')
                         
@@ -28,8 +36,11 @@ def get(path: str, headers: dict[str,str]) -> bytes:
                             response += (f'Connection: Keep-Alive\r\n')
                         
                         content_type = MIME_TYPES.get(requested_path.suffix, 'application/octet-stream')
-                        response += (f'Content-Length: {len(content)}\r\nContent-Type: {content_type}\r\n\r\n')
+                        response += (f'Content-Length: {len(content)}\r\nContent-Type: {content_type}\r\n')
+                        
+                        response += (f'ETag: "{current_etag}"\r\n')
 
+                        response += (f'\r\n')
                         response = response.encode('utf-8') + content
                         
                         return response
