c170345c210a50623fe8ebe96fd220cae7de10fa
David Meng <davidmenggx@gmail.com>
2026-01-14 09:59:03 -0800

    Cleaned up options, you dont need to open any file

--- handlers/options.py
+++ handlers/options.py
@@ -20,29 +20,25 @@ def options(path: str, headers: dict[str,str], body: bytes = b'') -> bytes:
         return responses.not_found()
 
     try:
-        with open(requested_path, 'rb') as f:
-            try:
-                now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
-                
-                response = (f"HTTP/1.1 204 No Content\r\n")
+        now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
+        
+        response = (f"HTTP/1.1 204 No Content\r\n")
 
-                if API_DIR in requested_path.parents or API_DIR == requested_path:
-                    response += (f'Allow: OPTIONS, GET, HEAD, POST\r\n')
-                else:
-                    response += (f'Allow: OPTIONS, GET, HEAD\r\n')
-                
-                response += (f"Date: {now}\r\nServer: David's server\r\n")
-                
-                if headers.get('connection', '') == 'close':
-                    response += (f'Connection: Close\r\n')
-                else:
-                    response += (f'Connection: Keep-Alive\r\n')
-                
-                response += (f'\r\n')
-                response = response.encode('utf-8')
-                
-                return response
-            except:
-                return responses.internal_server_error()
-    except FileNotFoundError:
-        return responses.not_found()
+        if API_DIR in requested_path.parents or API_DIR == requested_path:
+            response += (f'Allow: OPTIONS, GET, HEAD, POST\r\n')
+        else:
+            response += (f'Allow: OPTIONS, GET, HEAD\r\n')
+        
+        response += (f"Date: {now}\r\nServer: David's server\r\n")
+        
+        if headers.get('connection', '') == 'close':
+            response += (f'Connection: Close\r\n')
+        else:
+            response += (f'Connection: Keep-Alive\r\n')
+        
+        response += (f'\r\n')
+        response = response.encode('utf-8')
+        
+        return response
+    except:
+        return responses.internal_server_error()
